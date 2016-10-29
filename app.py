e9cc88e685822166c47cbd60ab9aa2ca1ca566a0
fukuta0614 <fukuta6140@gmail.com>
2016-10-29 15:22:03 +0900

    sqlite test

--- app.py
+++ app.py
@@ -3,14 +3,27 @@
 
 import os
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 app.debug = True
 
+cur_dir = os.path.dirname(__file__)
+dbpath = os.path.join(cur_dir, 'db.sqlite3')
+
+
+def sqlite_entry():
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    c.execute("select * from movies_movie")
+    x = " | ".join([str(x) for x in c.fetchone()])
+    conn.close()
+    return x
+
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return sqlite_entry()
 
 
 @app.route('/hello/<name>')
@@ -18,11 +31,6 @@ def hello(name=''):
     if name == '':
         name = 'ななしさん'
     return render_template('hello.html', name=name)
-
-
-@app.route('/debug')
-def debug():
-    return render_template('notemplate.html')
 
 
 if __name__ == '__main__':
