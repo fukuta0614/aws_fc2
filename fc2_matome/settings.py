ffee5aaf4c1e81ebd120ed74cfdfe359f13589fb
fukuta0614 <fukuta6140@gmail.com>
2016-10-29 21:53:54 +0900

    asdf

--- fc2_matome/settings.py
+++ fc2_matome/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn'
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'gunicorn'
 ]
 
 ROOT_URLCONF = 'fc2_matome.urls'
